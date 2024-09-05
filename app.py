b3ccd21677377e2b5120252f17360cf7bf79f056
Daniel Sardak <karate7800@gmail.com>
2024-09-05 15:22:07 -0400

    add title above boxes

--- app.py
+++ app.py
@@ -46,21 +46,50 @@ def respond(
 you bad
 
 """
-demo = gr.ChatInterface(
-    respond,
-    additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
-        ),
-    ],
-)
+
+# with gr.Blocks() as block:
+#     gr.Markdown(
+#         """
+#         # Welcome to the Chatbot App!
+#         This is a simple chatbot that uses the Zephyr-7B model from Hugging Face. 
+#         The chatbot can respond to your messages and generate new text based on your input. 
+#         You can also provide a system message, set the maximum number of tokens to generate, and adjust the temperature and top-p sampling parameters.
+#         """
+#     )
+    
+
+
+
+with gr.Blocks() as demo:
+
+    gr.Markdown(
+        '''
+    # Welcome to APp
+
+
+    '''
+    )
+
+    gr.ChatInterface(
+
+
+        respond,
+        additional_inputs=[
+            gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
+            gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
+            gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
+            gr.Slider(
+                minimum=0.1,
+                maximum=1.0,
+                value=0.95,
+                step=0.05,
+                label="Top-p (nucleus sampling)",
+            ),
+        ],
+    )
+
+
+
 
 
 if __name__ == "__main__":
