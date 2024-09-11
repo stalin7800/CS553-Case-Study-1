d1a6ebadacd895fd2fef4df1d867b1a55af66a1e
BradyA25 <156399490+BradyA25@users.noreply.github.com>
2024-09-11 14:48:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -169,14 +169,14 @@ with gr.Blocks(css=css) as demo:
         respond,
         additional_inputs=[
             gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-            gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-            gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
+            gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens: Length of response"),
+            gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature: Creativeness of the response"),
             gr.Slider(
                 minimum=0.1,
                 maximum=1.0,
                 value=0.95,
                 step=0.05,
-                label="Top-p (nucleus sampling)",
+                label="Top-p (nucleus sampling): Diversity of next token",
             ),
             gr.Checkbox(label='Use Local Model', value=False),
 
