dba81d71976e9cf2747db7af2fc18a6257b4111d
Daniel Sardak <108438025+stalin7800@users.noreply.github.com>
2024-09-15 14:54:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -62,13 +62,13 @@ def respond(
 
         yield response_html + response
 
-def respond_test():
+def test_respond():
     test = respond('Hello!',[],'You only respond hello.',5,1,1.5,False)
     
     with open('log.txt','w') as f:
         f.write(f'{type(test)}\n')
 
-    assert type(test) == dic
+    assert type(test) == dict
 
 
 # def respond(
